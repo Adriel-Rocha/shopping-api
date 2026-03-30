c2ffd9aa50d5b4efa666b2820c6766f2dc27c139
Adriel Francisco Rocha <adrielfr194@gmail.com>
2026-03-30 16:18:29 -0300

    feat: add `ShopRepository` interface

---
+++ src/main/java/com/adriel/shopping_api/repository/ShopRepository.java
@@ -0,0 +1,19 @@
+package com.adriel.shopping_api.repository;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.adriel.shopping_api.model.Shop;
+
+@Repository
+public interface ShopRepository extends JpaRepository<Shop, Long> {
+
+    public List<Shop> findAllByUserIdentifier(String userIdentifier);
+
+    public List<Shop> findAllByTotalGreaterThan(Float total);
+
+    public List<Shop> findAllByDateGreaterThanEquals(Date date);
+}
