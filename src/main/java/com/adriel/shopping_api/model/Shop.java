89e81d13403e6f5582905c771309fa14b3f8f42e
Adriel Francisco Rocha <adrielfr194@gmail.com>
2026-03-30 16:03:51 -0300

    feat: create `Shop` and `ShopItem` entities with relationships

---
+++ src/main/java/com/adriel/shopping_api/model/Shop.java
@@ -0,0 +1,39 @@
+package com.adriel.shopping_api.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity(name = "shop")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Shop {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String userIdentifier;
+
+    private BigDecimal total;
+
+    private LocalDateTime date;
+
+    @OneToMany(mappedBy = "shop", cascade = CascadeType.ALL)
+    private List<ShopItem> items;
+}
